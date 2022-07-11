c5273ea8c3184740a6f9be02f470296b500bd1c5
Alexander4649 <handyasu08@gmail.com>
2022-07-11 05:17:37 +0000

    [Start]初期設定:次はdb追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "b86b5109019d4858a23295e809a9fcb5.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
