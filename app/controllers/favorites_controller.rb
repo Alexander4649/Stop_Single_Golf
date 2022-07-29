463f8af421cb3150be6dd903d622e5dc41be812e
Alexander4649 <handyasu08@gmail.com>
2022-07-29 09:57:12 +0000

    [Add]いいね完成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,17 @@
 class FavoritesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    favorite = current_user.favorites.new(post_id: @post.id)
+    post_id = @post
+    favorite = current_user.favorites.new(post_id: post_id.id)
     favorite.save
-    redirect_to post_path(@post)
+    redirect_to request.referer
   end
 
   def destroy
     @post = Post.find(params[:post_id])
-    favorite = current_user.favorites.find_by(post_id: @post.id)
+    post_id = @post
+    favorite = current_user.favorites.find_by(post_id: post_id.id)
     favorite.destroy
-    redirect_to post_path(@post)
+    redirect_to request.referer
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   has_many :relationships, class_name: "Relationship", foreign_key: :follow_id, dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
   has_many :follows, through: :relationships, source: :follower
