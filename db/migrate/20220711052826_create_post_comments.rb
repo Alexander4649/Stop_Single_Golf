645055b933cacaeba5ce69b57de12316a20fd749
Alexander4649 <handyasu08@gmail.com>
2022-08-08 12:56:06 +0000

    post_comment変更

--- db/migrate/20220711052826_create_post_comments.rb
+++ db/migrate/20220711052826_create_post_comments.rb
@@ -1,9 +1,9 @@
 class CreatePostComments < ActiveRecord::Migration[6.1]
   def change
     create_table :post_comments do |t|
-      t.integer :user_id, null: false, default: ""
-      t.integer :post_id, null: false, default: ""
-      t.text :comment, null: false, default: ""
+      t.integer :user_id, null: false
+      t.integer :post_id, null: false
+      t.text :comment, null: false
 
       t.timestamps
     end
