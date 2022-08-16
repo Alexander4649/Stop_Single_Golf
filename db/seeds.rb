ed56ca56102bf985c0ad3f8c1f9239a59531a96a
Alexander4649 <handyasu08@gmail.com>
2022-08-16 09:25:59 +0000

    [Add]テストユーザー追加seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# adminユーザー
 User.create!(
   name: '管理者',
   email: 'admin@example.com',
@@ -13,4 +14,47 @@ User.create!(
   password_confirmation: 'admin1',
   admin: true
   # state_id: 45,
+)
+
+# テストユーザー
+User.create!(
+  name: 'TestUser1',
+  email: 'testuser1@example.com',
+  password: '111111',
+  password_confirmation: '111111',
+  round_area: '沖縄県',
+  average_score: '88',
+  experience: '8年8ヶ月',
+  # profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}./app/images/about.jpeg"), filename:"about"),
+  # profile_image: "./app/images/about.jpeg"
+)
+
+User.create!(
+  name: 'TestUser2',
+  email: 'testuser2@example.com',
+  password: 'test22',
+  password_confirmation: 'test22',
+  round_area: '北海道',
+  average_score: '111',
+  experience: '5年'
+)
+
+User.create!(
+  name: 'TestUser3',
+  email: 'testuser3@example.com',
+  password: 'test33',
+  password_confirmation: 'test33',
+  round_area: 'シドニー',
+  average_score: '94',
+  experience: '6年5ヶ月'
+)
+
+User.create!(
+  name: 'TestUser4',
+  email: 'testuser4@example.com',
+  password: 'test44',
+  password_confirmation: 'test44',
+  round_area: 'アテネ',
+  average_score: '135',
+  experience: '8ヶ月'
 )
